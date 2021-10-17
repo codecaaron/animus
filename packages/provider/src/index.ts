554f60c6ee7a720f49deadc0693bdd47e1452b2d
Aaron Robb <aaron@codecademy.com>
2021-10-17 18:32:40 -0400

    Sigh

--- packages/provider/src/index.ts
+++ packages/provider/src/index.ts
@@ -1,3 +1,11 @@
+import { theme } from '@animus/theme';
+
+export type AnimusTheme = typeof theme;
+
+declare module '@emotion/react' {
+  export interface Theme extends AnimusTheme {}
+}
+
 export * from './AnimusProvider';
 export * from './components';
 export * from './ComponentProvider';
