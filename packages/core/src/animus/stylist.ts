92a5e53c7503a7d1f5815442331e657650497189
codecaaron <raelaggro>
2021-12-19 07:31:49 -0500

    Refactor

--- packages/core/src/animus/stylist.ts
+++ packages/core/src/animus/stylist.ts
@@ -1,7 +1,10 @@
 import { isEmpty, isObject, pick, set } from 'lodash';
+import { CSSObject } from '..';
 import { AbstractParser } from '../types/config';
 
-type BasicStuff = Record<string, any>;
+interface AbstractStyleFnConfig {
+  [x: string]: any;
+}
 
 interface Rules {
   [x: string]: {
@@ -9,14 +12,10 @@ interface Rules {
   };
 }
 
-/**
- *
- */
-
 const getSelectors = (
-  base: BasicStuff = {},
-  variants: BasicStuff = {},
-  states: BasicStuff = {},
+  base: AbstractStyleFnConfig = {},
+  variants: AbstractStyleFnConfig = {},
+  states: AbstractStyleFnConfig = {},
   filters: string[]
 ) => {
   const rules: Rules = {};
@@ -74,6 +73,85 @@ const createIdGetter = (variants: any, states: any) => {
   };
 };
 
+const extractBreakpointStyles = (styles: CSSObject, medias: string[]) => {
+  const unscoped: CSSObject = {};
+  const breakpoint: CSSObject = {};
+  Object.entries(styles).forEach(([rule, value]) => {
+    const target = medias.includes(rule) ? breakpoint : unscoped;
+    target[rule] = value;
+  });
+  return [unscoped, breakpoint];
+};
+
+const applyOverride = (
+  override = {},
+  baseResult: any,
+  breakpointResult: any,
+  parser: any,
+  theme: any,
+  media: any
+) => {
+  const [overrideStyles, overrideBreakpointStyles] = extractBreakpointStyles(
+    parser({ ...override, theme }, true),
+    media
+  );
+
+  for (const rule in overrideStyles) {
+    baseResult[rule] = overrideStyles[rule];
+  }
+
+  media.forEach((mq: string) => {
+    const mqOverride = overrideBreakpointStyles[mq] as CSSObject;
+    if (!mqOverride || isEmpty(mqOverride)) return;
+
+    if (!breakpointResult[mq]) {
+      breakpointResult[mq] = {} as CSSObject;
+    }
+
+    const mqStyle = breakpointResult[mq] as CSSObject;
+
+    for (const rule in mqOverride) {
+      mqStyle[rule] = mqOverride[rule];
+    }
+  });
+};
+
+const applyStyle = (resultStyles: any, config: any, props: any, ctx: any) => {
+  const { base } = config;
+  const { parser, getMediaSelectors, getActiveOverrides } = ctx;
+  const { theme } = props;
+
+  const overrides = Object.values(pick(config, getActiveOverrides(props)));
+  const media = getMediaSelectors(props);
+
+  const [styles, breakpointStyles] = extractBreakpointStyles(
+    parser({ ...base, theme }, true),
+    media
+  );
+
+  for (const rule in styles) {
+    resultStyles[rule] = styles[rule];
+  }
+
+  overrides.forEach((override: {}) => {
+    applyOverride(
+      override,
+      resultStyles,
+      breakpointStyles,
+      parser,
+      theme,
+      media
+    );
+  });
+
+  media.forEach((media: string) => {
+    const bp = breakpointStyles[media];
+    if (!isEmpty(bp)) {
+      resultStyles[media] = bp;
+    }
+  });
+};
+
 export const stylist = (
   parser: AbstractParser,
   base: Record<string, any> = {},
@@ -81,118 +159,30 @@ export const stylist = (
   states: Record<string, any> = {},
   defaults: Record<string, any> = {}
 ) => {
-  const { primary = {}, ...selectors } = getSelectors(
-    base,
-    variants,
-    states,
-    parser.propNames
-  );
-  const getActiveStyleIds = createIdGetter(variants, states);
+  const selectorGroups = getSelectors(base, variants, states, parser.propNames);
+  const context = {
+    parser,
+    getMediaSelectors: ({ theme }: any) =>
+      ['xs', 'sm', 'md', 'lg', 'xl'].map((key) => theme.breakpoints[key]),
+    getActiveOverrides: createIdGetter(variants, states),
+  };
 
   return (props: any) => {
-    const { vars, theme } = props;
-    const media = ['xs', 'sm', 'md', 'lg', 'xl'].map(
-      (key) => theme.breakpoints[key]
-    );
-    const propStyles = parser(props);
-    const activeIds = getActiveStyleIds(props);
-    let result: any = {};
-    const { base, ...allOverrides } = primary;
-    const overrides: any = { ...pick(allOverrides, activeIds), propStyles };
+    const { vars } = props;
 
-    const {
-      [media[0]]: xs = {},
-      [media[1]]: sm = {},
-      [media[2]]: md = {},
-      [media[3]]: lg = {},
-      [media[4]]: xl = {},
-      ...styles
-    } = parser({ ...base, theme }, true);
-    result = { ...styles };
+    const result = { ...vars } as any;
 
-    Object.keys(overrides).forEach((id) => {
-      const {
-        [media[0]]: xsOverride,
-        [media[1]]: smOverride,
-        [media[2]]: mdOverride,
-        [media[3]]: lgOverride,
-        [media[4]]: xlOverride,
-        ...overrideStyles
-      } = parser({ ...overrides[id], theme }, true);
-
-      for (const rule in overrideStyles) {
-        result[rule] = overrideStyles[rule];
-      }
-
-      [
-        [xs, xsOverride],
-        [sm, smOverride],
-        [md, mdOverride],
-        [lg, lgOverride],
-        [xl, xlOverride],
-      ].forEach(([mqStyle, mqOverride]: any) => {
-        for (const rule in mqOverride) {
-          mqStyle[rule] = mqOverride[rule];
-        }
-      });
-    });
-
-    [xs, sm, md, lg, xl].forEach((bp, i) => {
-      if (!isEmpty(bp)) {
-        result[media[i]] = bp;
+    Object.entries(selectorGroups).forEach(([selectorId, config = {}]) => {
+      if (selectorId === 'primary') {
+        applyStyle(result, config, props, context);
+      } else {
+        result[selectorId] = {};
+        applyStyle(result[selectorId], config, props, context);
       }
     });
 
-    Object.entries(selectors).forEach(([secondarySelector, config = {}]) => {
-      const { base, ...overrides } = config;
-      const {
-        [media[0]]: xs = {},
-        [media[1]]: sm = {},
-        [media[2]]: md = {},
-        [media[3]]: lg = {},
-        [media[4]]: xl = {},
-        ...styles
-      } = parser({ ...base, theme }, true);
-      const secondaryResult = { ...styles };
-      const selectorOverrides = pick(overrides, activeIds);
+    applyStyle(result, { base: parser(props) }, props, context);
 
-      Object.keys(selectorOverrides).forEach((id) => {
-        const {
-          [media[0]]: xsOverride,
-          [media[1]]: smOverride,
-          [media[2]]: mdOverride,
-          [media[3]]: lgOverride,
-          [media[4]]: xlOverride,
-          ...overrideStyles
-        } = parser({ ...selectorOverrides[id], theme }, true);
-
-        for (const rule in overrideStyles) {
-          secondaryResult[rule] = overrideStyles[rule];
-        }
-
-        [
-          [xs, xsOverride],
-          [sm, smOverride],
-          [md, mdOverride],
-          [lg, lgOverride],
-          [xl, xlOverride],
-        ].forEach(([mqStyle = {}, mqOverride]: any) => {
-          for (const rule in mqOverride) {
-            mqStyle[rule] = mqOverride[rule];
-          }
-        });
-      });
-
-      [xs, sm, md, lg, xl].forEach((bp, i) => {
-        if (!isEmpty(bp)) {
-          secondaryResult[media[i]] = bp;
-        }
-      });
-
-      if (!isEmpty(secondaryResult)) {
-        result[secondarySelector] = secondaryResult;
-      }
-    });
-    return { ...vars, ...result };
+    return result;
   };
 };
