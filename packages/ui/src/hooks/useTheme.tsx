f16b8cdc2eefd7c72f5b260348baf22da72f5e29
codecaaron <raelaggro@gmail.com>
2022-01-16 10:29:50 -0500

    Add mode compatible theme

--- packages/ui/src/hooks/useTheme.tsx
+++ packages/ui/src/hooks/useTheme.tsx
@@ -1,12 +1,14 @@
 import { CompatTheme } from '@animus-ui/core';
 import { Theme, useTheme as useEmotionTheme } from '@emotion/react';
 
-export interface CompatTheme extends Theme, Omit<CompatTheme, keyof Theme> {
+export interface ModeCompatibleTheme
+  extends Theme,
+    Omit<CompatTheme, keyof Theme> {
   _getColorValue: (arg: any) => any;
 }
 
 export const useTheme = () => {
   const emotionTheme = useEmotionTheme();
 
-  return emotionTheme as CompatTheme;
+  return emotionTheme as ModeCompatibleTheme;
 };
