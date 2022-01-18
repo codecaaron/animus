c71e4d2f5d0f57c3451e9a2f39e412ca169d2f9a
codecaaron <raelaggro@gmail.com>
2022-01-18 03:08:42 -0500

    Add checks

--- packages/ui/src/theme.tsx
+++ packages/ui/src/theme.tsx
@@ -2,10 +2,13 @@ import { AbstractTheme } from '@animus-ui/core';
 
 type Colors<T = {}> = {
   primary: T;
+  'primary-hover': T;
   secondary: T;
+  'secondary-hover': T;
   scrollbar: T;
   text: T;
   background: T;
+  'background-current': T;
 };
 
 export interface ModeCompatibleTheme extends AbstractTheme {
@@ -15,7 +18,7 @@ export interface ModeCompatibleTheme extends AbstractTheme {
     dark: Colors<keyof Colors>;
   };
   mode: 'light' | 'dark';
-  _getColorValue: (arg: any) => any;
+  _getColorValue: (arg: keyof Colors) => string;
 }
 declare module '@emotion/react' {
   export interface Theme extends ModeCompatibleTheme {}
