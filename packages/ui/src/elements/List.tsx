eefd4dadbd8e64e93ebbfc9d59d172a785b91e11
codecaaron <raelaggro@gmail.com>
2022-02-02 03:20:15 -0500

    Fix

---
+++ packages/ui/src/elements/List.tsx
@@ -0,0 +1,43 @@
+import { animus } from '@animus-ui/core';
+
+export const Ul = animus
+  .styles({ mb: 16, pl: 20 })
+  .states({
+    plain: {
+      m: 0,
+      p: 0,
+    },
+  })
+  .groups({
+    layout: true,
+    color: true,
+    positioning: true,
+    space: true,
+  })
+  .asComponent('ul');
+
+export const Ol = animus
+  .styles({ mb: 16, pl: 20 })
+  .states({
+    plain: {
+      m: 0,
+      p: 0,
+    },
+  })
+  .groups({
+    layout: true,
+    color: true,
+    positioning: true,
+    space: true,
+  })
+  .asComponent('ol');
+
+export const Li = animus
+  .styles({ m: 0 })
+  .groups({
+    layout: true,
+    typography: true,
+    color: true,
+    space: true,
+  })
+  .asComponent('li');
--- packages/ui/src/globals/Reset.tsx
+++ packages/ui/src/globals/Reset.tsx
@@ -64,7 +64,7 @@ export const Reset = () => {
         body {
           margin: 0;
           font-weight: 400;
-          line-height: 1.5;
+          line-height: 1.625;
           text-align: left;
         }
 
