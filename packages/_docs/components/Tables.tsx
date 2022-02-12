f755075074bd8ed9c09929a09f830e2dee6abef1
codecaaron <raelaggro@gmail.com>
2022-02-11 20:18:21 -0500

    Save Scale table

--- packages/_docs/components/Tables.tsx
+++ packages/_docs/components/Tables.tsx
@@ -1,37 +1,15 @@
-import { Text } from '@animus-ui/components';
-import {
-  animus,
-  borderShorthand,
-  compatTheme,
-  numberToPx,
-} from '@animus-ui/core';
+import { animus, compatTheme } from '@animus-ui/core';
 import { Prop } from '@animus-ui/core/dist/types/config';
-import {
-  get,
-  identity,
-  isArray,
-  isEmpty,
-  isNumber,
-  kebabCase,
-  size,
-} from 'lodash';
-import { Fragment } from 'react';
+import { kebabCase } from 'lodash';
+import { Fragment, useMemo } from 'react';
 
 import { Code } from './Code';
-
-const transforms = {
-  radii: size,
-  borders: borderShorthand,
-  spacing: numberToPx,
-  fontSize: numberToPx,
-  breakpoints: numberToPx,
-};
 
 const Table = animus
   .styles({
     my: 16,
     mb: 24,
-    fontSize: 14,
+    fontSize: 16,
     borderCollapse: 'collapse',
     width: 1,
   })
@@ -75,31 +53,30 @@ export const PropTable = ({
         <TableHeader>Prop</TableHeader>
         <TableHeader>Properties</TableHeader>
         <TableHeader>Scale</TableHeader>
-        <TableHeader>Transform</TableHeader>
       </TableRow>
       {animus.groupRegistry[group].map((prop) => {
         const {
           property,
           properties = [property],
           scale,
-          transform,
         } = animus.propRegistry[prop] as Prop;
 
         return (
           <TableRow key={prop}>
-            <TableCell>{prop}</TableCell>
+            <TableCell>
+              <Code>{prop}</Code>
+            </TableCell>
             <TableCell>
               {properties.map(kebabCase).map((pn, i, arr) => (
                 <Fragment key={pn}>
-                  <Code key={pn}>
-                    <Text fontSize={14}>{pn}</Text>
-                  </Code>
+                  <Code key={pn}>{pn}</Code>
                   {arr.length !== i + 1 ? ', ' : ''}
                 </Fragment>
               ))}
             </TableCell>
-            <TableCell>{scale}</TableCell>
-            <TableCell>{transform?.name}</TableCell>
+            <TableCell>
+              <Code>{scale}</Code>
+            </TableCell>
           </TableRow>
         );
       })}
@@ -107,48 +84,46 @@ export const PropTable = ({
   );
 };
 
-export const ScaleTable = ({ scale }: { scale: keyof typeof compatTheme }) => {
-  const values = compatTheme[scale];
-  const sorted =
-    isArray(values) && isNumber(parseInt(`${values[0]}`, 10))
-      ? values.sort((a, b) => (a > b ? 1 : -1))
-      : values;
-
-  const transform = get(transforms, scale, identity);
-
+export const ScaleTable = () => {
+  const propertiesByScale = useMemo(() => {
+    const values = Object.keys(compatTheme).filter(
+      (key) => !['breakpoints', 'mode', 'modes'].includes(key)
+    );
+    const props = Object.values(animus.propRegistry);
+    return values.map((scaleKey) => {
+      const properties = props.reduce((carry, config: Prop) => {
+        if (config.scale !== scaleKey) return carry;
+        const propertyNames = config.properties
+          ? [...config.properties, config.property]
+          : [config.property];
+        return carry.concat(propertyNames);
+      }, []);
+      return { scale: scaleKey, properties };
+    });
+  }, []);
   return (
     <Table>
       <TableRow header>
-        <TableHeader>Alias</TableHeader>
-        <TableHeader>Value</TableHeader>
+        <TableHeader>Scale</TableHeader>
+        <TableHeader>Properties</TableHeader>
       </TableRow>
-      {isArray(sorted)
-        ? sorted.map((value) => {
-            return (
-              <TableRow key={value}>
-                <TableCell>
-                  <Code>{value}</Code>
-                </TableCell>
-                <TableCell>
-                  <Code>{transform(value)}</Code>
-                </TableCell>
-              </TableRow>
-            );
-          })
-        : Object.entries(sorted).map(([scaleKey, value]) => {
-            if (isEmpty(sorted)) return null;
-
-            return (
-              <TableRow key={scaleKey}>
-                <TableCell>
-                  <Code>{scaleKey}</Code>
-                </TableCell>
-                <TableCell>
-                  <Code>{transform(value)}</Code>
-                </TableCell>
-              </TableRow>
-            );
-          })}
+      {propertiesByScale.map(({ scale, properties }) => {
+        return (
+          <TableRow key={scale}>
+            <TableCell>
+              <Code>{scale}</Code>
+            </TableCell>
+            <TableCell>
+              {properties.map(kebabCase).map((pn, i, arr) => (
+                <Fragment key={pn}>
+                  <Code key={pn}>{pn}</Code>
+                  {arr.length !== i + 1 ? ', ' : ''}
+                </Fragment>
+              ))}
+            </TableCell>
+          </TableRow>
+        );
+      })}
     </Table>
   );
 };
