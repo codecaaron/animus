06736e709b06423afaf2ff5b9b237732fba70841
codecaaron <raelaggro@gmail.com>
2023-03-11 22:02:58 -0500

    Rollup v3

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,26 @@
-import typescript from 'rollup-plugin-typescript2';
-import babel from '@rollup/plugin-babel';
+const typescript = require('rollup-plugin-typescript2');
+const babel = require('@rollup/plugin-babel');
 
-const config = () => {
-  const { main, dependencies } = require(`${__dirname}/package.json`);
-  return {
-    input: `${__dirname}/src/index.ts`,
-    output: [
-      {
-        file: main,
-        format: 'es',
-      },
-    ],
-    external: [...Object.keys(dependencies || {})],
-    plugins: [
-      typescript({
-        typescript: require('typescript'),
-      }),
-      babel({
-        extensions: ['tsx', 'ts'],
-        exclude: './node_modules/**',
-        babelHelpers: 'bundled',
-      }),
-    ],
-  };
+module.exports = {
+  input: `./src/index.ts`,
+  output: [
+    {
+      file: './dist/index.mjs',
+      format: 'es',
+    },
+    {
+      file: './dist/index.js',
+      format: 'cjs',
+    },
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+    babel({
+      extensions: ['tsx', 'ts'],
+      exclude: './node_modules/**',
+      babelHelpers: 'bundled',
+    }),
+  ],
 };
-export default config;
