0abf388ea023c59d4cf19c8d4d26085df87e4d6c
codecaaron <raelaggro@gmail.com>
2026-09-06 11:37:34 -0400

    Save

--- e2e/vinext-app/next-env.d.ts
+++ e2e/vinext-app/next-env.d.ts
@@ -1,5 +1,5 @@
-import "vinext/types";
-import "./.next/types/routes.d.ts";
+import 'vinext/types';
+import './.next/types/routes.d.ts';
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
